651505d8e034acbe2516e123172c44b5174a3c99
Emily Kapor <emily@emilyaviva.com>
2015-06-13 17:46:18 -0700

    Updated all working files.

--- app/client.js
+++ app/client.js
@@ -2,30 +2,52 @@
 
 $(function () {
 
-	$('form').on('submit', function (event) {
+	$('#input-adjective').on('submit', function (event) {
 		console.log(event);
 		event.preventDefault();
 		var userAdjective = $('#user-adjective').val();
 		$.post('/adjective', {word: userAdjective}, function (response) {
-			var confirm = response.message +  " <em>" + response.confirm + "</em>";
-			$('#adjective-response').html(confirm);
-		})
-	})
+			var confirm = response.message + " <em>" + response.confirm + "</em>";
+			$('#word-add-response').html(confirm);
+		});
+	};
 
-	$("button").click(function() {
+	$('#input-verb').on('submit', function (event) {
+		console.log(event);
+		event.preventDefault();
+		var userVerb = $('#user-verb').val();
+		$.post('/verb', {word: userVerb}, function (response) {
+			var confirm = response.message + " <em>" + response.confirm + "</em>";
+			$('#word-add-response').html(confirm);
+		});
+	};
+
+	$('#input-noun').on('submit', function (event) {
+		console.log(event);
+		event.preventDefault();
+		var userAdjective = $('#user-adjective').val();
+		$.post('/noun', {word: userNoun}, function (response) {
+			var confirm = response.message + " <em>" + response.confirm + "</em>";
+			$('#word-add-response').html(confirm);
+		});
+	};
+
+	$('#get-name').click(function() {
 		$.get('/adjective', function (response) {
-			var adjective = resonse.word;
+			var adjective = response.word;
 			$("#adjective").text(adjective);
 		});
 
 		$.get('/verb', function (response) {
-			var verb = resonse.word;
+			var verb = response.word;
 			$("#verb").text(verb);
 		});
 
 		$.get('/noun', function (response) {
-			var noun = resonse.word;
+			var noun = response.word;
 			$("#noun").text(noun);
 		});
+
+		$('#band-name').html(adjective + verb + noun);
 	});
-});
+})
--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var getRandomWord = require("./lib/getRandomWord");
+var postWord = require("./lib/postWord");
+
 var Adjective = require("./lib/adjective");
+var Noun = require("./lib/noun");
+var Verb = require("./lib/verb");
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -11,53 +15,38 @@ app.use(express.static(__dirname + "/app/"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-var Noun = function () {
-	this.Tables = true;
-	this.Laptops = true;
-	this.Shirts = true;
-	this.Presidents = true;
-	this.Genders = true;
-};
+var adjective = new Adjective();
 var noun = new Noun();
+var verb = new Verb();
 
-var Verb = function () {
-	this.Kick = true;
-	this.Bring = true;
-	this.Die = true;
-	this.Love = true;
-	this.Connect = true;
-	this.Slay = true;
-};
-var verb = new Verb;
-
-var adjective = new Adjective;
-
-function postWord (word, wordObject) {
-	var msg;
-	if (wordObject.hasOwnProperty(word)) {
-		msg = "We already have your word: ";
-	} else {
-		wordObject[word] = true;
-		msg = "We saved your word: ";
-	}
-	return {message: msg, confirm: word};
-}
 
 app.post("/adjective", function (req, res) {
-	res.json(postWord(req.body.word, adjective));
+	res.json(postWord(req.body.word, 'adjective', adjective));
 });
 
 app.get("/adjective", function (req, res) {
 	getRandomWord(adjective);
 });
 
+app.post("/verb", function (req, res) {
+	res.json(postWord(req.body.word, 'verb', verb));
+});
+
 app.get("/verb", function (req, res) {
-	getRandomWord(verb);  
+	getRandomWord(verb);
+});
+
+app.post("/noun", function (req, res) {
+	res.json(postWord(req.body.word, 'noun', noun));
 });
 
 app.get("/noun", function (req, res) {
 	getRandomWord(noun);
 });
+
+
+
+
 
 app.listen(port, function () {
 	console.log('server started on port', port);
